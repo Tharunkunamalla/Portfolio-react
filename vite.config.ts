48006fd36d8226ce9918f403ce44db8062c8ace3
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-06-01 20:26:26 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,5 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
-  base: process.env.VITE_BASE_PATH ?? "/Portfolio-react",
+  // base: process.env.VITE_BASE_PATH ?? "/Portfolio-react",
 });
